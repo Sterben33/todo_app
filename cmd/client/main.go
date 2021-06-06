1c5819668b661ff7d754093bbd1c2cab1deacefc
Sterben33 <nikitka.gorbunov.1976@mail.ru>
2021-06-06 17:57:27 +0300

    Basic functionality with bugs

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
 	"log"
+	"os"
 	"strconv"
 	"todo_app/pkg/api"
 )
 
+var input string
+
+func GetId() (int32, error){
+
+
+
+	//Getting id
+	fmt.Println("Input id of the task. ")
+	_, err1 := fmt.Scanf("%s", &input)
+	if err1 != nil { return -1, err1 }
+	id, err2 := strconv.Atoi(input)
+	if err2 != nil { return -1, err2 }
+
+	return int32(id), nil
+}
+
+func GetData() (string, string, error){
+
+	reader := bufio.NewReader(os.Stdin)
+	//Getting title
+	fmt.Print("Input new title: ")
+	title, _ := reader.ReadString('\n')
+
+	//Getting body
+	fmt.Print("Input new body: ")
+	body, _ := reader.ReadString('\n')
+
+	return title, body, nil
+}
 func main() {
 
 	conn, err := grpc.Dial(":5000", grpc.WithInsecure())
@@ -18,34 +49,108 @@ func main() {
 	defer conn.Close()
 
 	c := api.NewToDoClient(conn)
-
-	for {
-		fmt.Println("Choose an option:\n " +
-			"list - get list of the tasks.\n " +
+	b := true
+	for b {
+		fmt.Println("Choose an option:\n" +
+			"list - get list of the tasks.\n" +
 			"create - create a new task.\n" +
 			"read - read task by id.\n" +
 			"update - update an information in task.\n" +
 			"delete - delete task by id.\n" +
-			"done - mark as completed by id.\n ")
+			"done - mark as completed by id.\n" +
+			"q - quit. ")
+
+		_, err = fmt.Scanf("%s", &input)
+		if err != nil {
+			fmt.Println(err)
+		}
+		option := input
+		fmt.Println()
+		switch option{
+			case "list":
+				res, e := c.GetList(context.Background(), &api.Empty{})
+				if e != nil {
+					fmt.Println(err)
+					continue
+				}
+				for task := range res.Task {
+					fmt.Printf("%v %v %v %v\n",res.Task[task].GetId(),res.Task[task].GetTitle(),res.Task[task].GetBody(),res.Task[task].GetDone())
+					fmt.Println("--------------------")
+				}
+
+			case "create":
+				title, body, err := GetData()
+				if err != nil {
+					fmt.Println(err)
+				}
+				res, e := c.Create(context.Background(), &api.Data{Title: title, Body: body})
+				if e != nil {
+					fmt.Println(e)
+					continue
+				}
+				fmt.Println(res.GetBody())
+			case "read":
+				id, err := GetId()
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				res, e := c.Read(context.Background(), &api.TaskId{Id: id})
+				if e != nil {
+					fmt.Println(err)
+					continue
+				}
+				fmt.Println(res.GetBody())
+			case "update":
+				id, er := GetId()
+				if er != nil {
+					fmt.Println(er)
+					continue
+				}
+				title, body, err := GetData()
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				res, e := c.Update(context.Background(), &api.Task{Id: id,Title: title, Body: body})
+				if e != nil {
+					fmt.Println(e)
+					continue
+				}
+				fmt.Println(res)
+			case "delete":
+				id, err := GetId()
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				_, e := c.Delete(context.Background(), &api.TaskId{Id: id})
+				if e != nil {
+					fmt.Println(e)
+					continue
+				}
+				fmt.Println("Deleted successfully. ")
+			case "done":
+				id, err := GetId()
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				res, e := c.MarkAsDone(context.Background(), &api.TaskId{Id: id})
+				if e != nil {
+					fmt.Println(e)
+					continue
+				}
+				fmt.Printf("%v", res.GetBody())
+
+			case "q":
+				b = false
+		}
+		fmt.Println()
+		fmt.Println()
+		if !b {
+			break
+		}
 	}
 
-	fmt.Println("Input id of the task. ")
-	var inputstr string
-
-	_, err = fmt.Scanf("%s", &inputstr)
-	if err != nil {
-		fmt.Println(err)
-	}
-	id, e := strconv.Atoi(inputstr)
-	if e != nil {
-		fmt.Println(e)
-	}
-
-
-	res, err := c.Read(context.Background(), &api.TaskId{Id: int32(id)})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Printf("Re %s",res.GetBody())
 }
--- pkg/todo/grpcserver.go
+++ pkg/todo/grpcserver.go
@@ -8,6 +8,10 @@ import (
 
 type GRPCServer struct {}
 
+var TaskMap = make(map[int32]Task)
+
+var Counter int32 = 0
+
 type Task struct {
 	id int32
 	title string
@@ -15,29 +19,25 @@ type Task struct {
 	done bool
 }
 
-var TaskMap = make(map[int32]Task)
-
-var Counter int32 = 0
-
 func NewTask(Title,Body string) Task {
 	defer func() {Counter++}()
 	TaskMap[Counter] = Task{id: Counter, title:Title, body: Body, done: false}
 	return TaskMap[Counter]
 }
 
-func (s *GRPCServer) Create(ctx context.Context, request *api.Data) (*api.Task, error) {
+func (s *GRPCServer) Create(_ context.Context, request *api.Data) (*api.Task, error) {
 	t := NewTask(request.Title, request.Body)
 	return &api.Task{Id: t.id, Title: t.title, Body: t.body, Done: t.done}, nil
 }
 
-func (s *GRPCServer) Read(ctx context.Context, request *api.TaskId) (*api.Task, error) {
+func (s *GRPCServer) Read(_ context.Context, request *api.TaskId) (*api.Task, error) {
 	if t, exist := TaskMap[request.GetId()]; exist {
 		return &api.Task{Id: t.id, Title: t.title, Body: t.body, Done: t.done}, nil
 	}
 	return &api.Task{}, errors.New("Task with such ID does not exist. ")
 }
 
-func (s *GRPCServer) Update(ctx context.Context, request *api.Task) (*api.Task, error) {
+func (s *GRPCServer) Update(_ context.Context, request *api.Task) (*api.Task, error) {
 	if t, exist := TaskMap[request.GetId()]; exist {
 		TaskMap[request.GetId()] = Task{id: t.id, title: request.GetTitle(), body: request.GetBody(), done: t.done}
 		return &api.Task{Id: t.id, Title: request.GetTitle(), Body: request.GetBody(), Done: t.done}, nil
@@ -45,7 +45,7 @@ func (s *GRPCServer) Update(ctx context.Context, request *api.Task) (*api.Task, 
 	return &api.Task{}, errors.New("Task with such ID does not exist. ")
 }
 
-func (s *GRPCServer) Delete(ctx context.Context, request *api.TaskId) (*api.Empty, error) {
+func (s *GRPCServer) Delete(_ context.Context, request *api.TaskId) (*api.Empty, error) {
 	if _, exist := TaskMap[request.GetId()]; exist {
 		delete(TaskMap, request.GetId())
 		return &api.Empty{}, nil
@@ -53,7 +53,7 @@ func (s *GRPCServer) Delete(ctx context.Context, request *api.TaskId) (*api.Empt
 	return &api.Empty{}, errors.New("Task with such ID does not exist. ")
 }
 
-func (s *GRPCServer) MarkAsDone(ctx context.Context, request *api.TaskId) (*api.Task, error) {
+func (s *GRPCServer) MarkAsDone(_ context.Context, request *api.TaskId) (*api.Task, error) {
 	if t, exist := TaskMap[request.GetId()]; exist {
 		t.done = true
 		TaskMap[request.GetId()] = t
@@ -62,8 +62,8 @@ func (s *GRPCServer) MarkAsDone(ctx context.Context, request *api.TaskId) (*api.
 	return &api.Task{}, errors.New("Task with such ID does not exist. ")
 }
 
-func (s *GRPCServer) GetList(ctx context.Context, request *api.Empty) (*api.TaskList, error) {
-	l := make([]*api.Task,len(TaskMap))
+func (s *GRPCServer) GetList(_ context.Context, _ *api.Empty) (*api.TaskList, error) {
+	l := make([]*api.Task,0,len(TaskMap))
 	for _, task := range TaskMap {
 		l = append(l, &api.Task{Id: task.id, Title: task.title, Body: task.body, Done: task.done})
 	}
